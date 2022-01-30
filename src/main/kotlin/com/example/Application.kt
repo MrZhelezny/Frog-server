aaa5389ca4b138523e1ca68a67945fb85b5c2edc
Yury Zhalezny <kvakazabr@gmail.com>
2022-01-30 23:50:22 +0300

    проект настроен на localhost через файл конфигурации

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,10 +3,21 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSerialization()
-    }.start(wait = true)
+    embeddedServer(Netty, environment = applicationEngineEnvironment {
+        config = HoconApplicationConfig(ConfigFactory.load())
+
+        module {
+            configureRouting()
+            configureSerialization()
+        }
+
+        connector {
+            port = config.property("ktor.deployment.port").getString().toInt()
+            host = config.property("ktor.deployment.host").getString()
+        }
+    }).start(wait = true)
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.model.User
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
@@ -8,9 +9,20 @@ import io.ktor.request.*
 
 fun Application.configureRouting() {
 
+    var users = mutableListOf<User>()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/api/") {
+            call.respondText("Hello Platon!")
+        }
+        post("/api/") {
+            val customer = call.receive<User>()
+            users.add(customer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
         }
     }
 }
