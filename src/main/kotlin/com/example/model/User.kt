4d6cccf737bfc1d2401bdbf2d28412bb4b3a05e4
zya <mr.zhelezny@mail.ru>
2022-04-11 23:25:21 +0300

    0.0.3: разделение на два get запроса. один на получение id, второй на ожидание игры

--- src/main/kotlin/com/example/model/User.kt
+++ src/main/kotlin/com/example/model/User.kt
@@ -1,6 +1,6 @@
 package com.example.model
 
-data class User(val id: String, var status: Enum<UserStatus>)
+data class User(val id: String, val nickName: String, var status: Enum<UserStatus>)
 
 enum class UserStatus {
     ONLINE
